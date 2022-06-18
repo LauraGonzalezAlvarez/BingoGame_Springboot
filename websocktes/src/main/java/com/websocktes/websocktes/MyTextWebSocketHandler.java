a3ccc30448626e07fc1ead0547392d7b7ec3b4ba
Santiago <porqueamisg@gmail.com>
2022-06-18 18:14:58 -0500

    Se sube codigo existente

---
+++ websocktes/src/main/java/com/websocktes/websocktes/MyTextWebSocketHandler.java
@@ -0,0 +1,41 @@
+package com.websocktes.websocktes;
+
+import org.attoparser.dom.Text;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class MyTextWebSocketHandler extends TextWebSocketHandler {
+	
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        super.afterConnectionClosed(session, status);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        sessions.forEach(webSocketSession -> {
+            try {
+            	bingo1 bn = new bingo1();
+            	String y = bn.messa();
+                webSocketSession.sendMessage(new TextMessage(y));
+            } catch (IOException e) {
+            }
+        });
+    }
+}
